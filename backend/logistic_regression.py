a7936ea621a36be4134eaa48e6608d0795682f56
isaacpopoola <34727430+isaacpopoola@users.noreply.github.com>
2021-04-07 19:06:45 -0400

    added file for our implmentation of logistic regression
    Co-authored-by: emiryy <emiryy@users.noreply.github.com>
    Co-authored-by: Temitayo Oyelowo <Temitayooyelowo@users.noreply.github.com>

---
+++ backend/logistic_regression.py
@@ -0,0 +1,18 @@
+from math import loglp
+import numpy as np
+import pandas as pd
+
+class LogisticRegression:
+
+    def sigmoid(self):
+        pass
+
+    def train(self):
+        pass
+
+    def accuracy_evaluation(self):
+        pass
+
+    def predict(self):
+        pass
+    
