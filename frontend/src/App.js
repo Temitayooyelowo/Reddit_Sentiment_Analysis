b600bfaa014645a66ca010758c2b1bfdb58cb7f3
Emily Ye <emily.ye@shopify.com>
2021-04-13 16:57:19 -0400

    ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,68 +1,34 @@
 import React, { useEffect, useState } from "react";
 import Search from "./components/Search";
-import {Container } from "semantic-ui-react";
+import {Container, Header } from "semantic-ui-react";
 import "./App.css";
-
-// const stocks = [
-//   { id: "1", name: "Amazon", sentiment: -1 },
-//   { id: "2", name: "Shopify", sentiment: +1 },
-//   { id: "3", name: "Google", sentiment: 0 },
-//   { id: "4", name: "Facebook", sentiment: 1 },
-// ];
-
-// const filterStocks = (stocks, query) => {
-//   if (!query) {
-//     return stocks;
-//   }
-
-//   return stocks.filter((post) => {
-//     const postName = post.name.toLowerCase();
-//     return postName.includes(query);
-//   });
-// };
-
-
-// const App = () => {
-//   const { search } = window.location;
-//   const query = new URLSearchParams(search).get("s");
-//   const [searchQuery, setSearchQuery] = useState(query || "");
-//   const filteredStocks = filterStocks(stocks, searchQuery);
-
-//   return (
-//     <div>
-//       <Search searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
-//       {/* <Dropdown options={options} value={defaultOption} placeholder="Select an option" />; */}
-//       <ul>
-//         {filteredStocks.map((stock) => (
-//           <p key={stock.key}>
-//             {stock.name}
-//             <Sentiment sentiment={stock.sentiment} />
-//           </p>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// };
 
 function App() {
   // const { search } = window.location;
   // const query = new URLSearchParams(search).get("s");
   // const [searchQuery, setSearchQuery] = useState(query || "");
-  const [stocks, setStocks] = useState([]);
-  const [sentiment, setSentiment] = useState([]);
+  // const [stocks, setStocks] = useState([]);
+  // const [sentiment, setSentiment] = useState([]);
 
-  useEffect(() => {
-    fetch("http://127.0.0.1:5000/sentiment").then(response =>
-    response.json().then(data => {
-      setStocks(data.sentiment)
-    })
-    );
-  }, []);
+  // useEffect(() => {
+  //   fetch("http://127.0.0.1:5000/sentiment").then(response =>
+  //   response.json().then(data => {
+  //     setStocks(data.sentiment)
+  //   })
+  //   );
+  // }, []);
 
   return (
-    <Container style={{alignItems: 'center'}}>
-      <Search/>
-    </Container>
+    <>
+      <div>
+        <Header as='h1'>Reddit Sentiment Analysis</Header>
+      </div>
+      <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '25vh'}}>
+        <Container style={{alignItems: 'center'}}>
+          <Search/>
+        </Container>
+      </div>
+    </>
   )
 }
 
--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -1,22 +1,24 @@
 import React, { useState } from "react";
 import { Button, Dropdown, Form, Input } from "semantic-ui-react";
 
-// const options = [
-//   { value: 'past year', text: 'Past year' },
-//   { value: 'past 3 months', text: 'Past 3 months'},
-//   { value: 'past month', text: "Past month"},
-//   { value: 'past week', text: "Past week"},
-// ];
+const options = [
+  { value: 'past year', text: 'Past year' },
+  { value: 'past 3 months', text: 'Past 3 months'},
+  { value: 'past month', text: "Past month"},
+  { value: 'past week', text: "Past week"},
+];
 
-// const defaultOption = options[2];
+// const { time } = this.state;
 
-// const TimeDropdown = () => (
-// <Dropdown
-//   placeholder='Select time'
-//   selection
-//   options={options}
-// />
-// )
+const TimeDropdown = () => (
+<Dropdown
+  placeholder='Select time'
+  selection
+  options={options}
+  // onChange={this.handleChange}
+  // value={time}
+/>
+)
 
 const Search = () => {
   const [stock, setStock] = useState('');
@@ -25,27 +27,30 @@ const Search = () => {
 
   return (
     <Form>
-      <Form.Field>
-        <Input 
-        placeholder="Search stocks"
-        value={stock}
-        onChange={e => setStock(e.target.value)}
-        name="s"
-        />
-      </Form.Field>
-      <Form.Field>
-        <Button onClick={async () => {
-            const query = {time, stock};
-            const response = await fetch('http://127.0.0.1:5000/sentiment', {
-              method: 'GET',
-              body: JSON.stringify(query)
-            })
+      <Form.Group>
+        <Form.Field>
+          <Input 
+          placeholder="Search stocks"
+          value={stock}
+          onChange={e => setStock(e.target.value)}
+          name="s"
+          />
+        </Form.Field>
+        <Form.Field>
+          <TimeDropdown/>
+          <Button onClick={async () => {
+              const query = {time, stock};
+              const response = await fetch('http://127.0.0.1:5000/sentiment', {
+                method: 'GET',
+                body: JSON.stringify(query)
+              })
 
-            if (response.ok) {
-              console.log("response worked")
-            }
-        }}>Submit</Button>
-      </Form.Field>
+              if (response.ok) {
+                console.log(response)
+              }
+          }}>Search</Button>
+        </Form.Field>
+      </Form.Group>
     </Form>
   )
 }
