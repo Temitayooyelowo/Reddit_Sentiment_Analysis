61272fa0e4da07e8ad165dddf54395acdc137239
Emily Ye <emily.ye@shopify.com>
2021-04-13 16:30:26 -0400

    wip

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,55 +1,69 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Search from "./components/Search";
-import Sentiment from "./components/Sentiment";
+import {Container } from "semantic-ui-react";
 import "./App.css";
 
-const stocks = [
-  { id: "1", name: "Amazon", sentiment: -1 },
-  { id: "2", name: "Shopify", sentiment: +1 },
-  { id: "3", name: "Google", sentiment: 0 },
-  { id: "4", name: "Facebook", sentiment: 1 },
-];
-
-const filterStocks = (stocks, query) => {
-  if (!query) {
-    return stocks;
-  }
-
-  return stocks.filter((post) => {
-    const postName = post.name.toLowerCase();
-    return postName.includes(query);
-  });
-};
-
-// const options = [
-//   { value: 'past year', label: 'Past year' },
-//   { value: 'past 3 months', label: 'Past 3 months'},
-//   { value: 'past month', label: "Past month"},
-//   { value: 'past week', label: "Past week"},
+// const stocks = [
+//   { id: "1", name: "Amazon", sentiment: -1 },
+//   { id: "2", name: "Shopify", sentiment: +1 },
+//   { id: "3", name: "Google", sentiment: 0 },
+//   { id: "4", name: "Facebook", sentiment: 1 },
 // ];
 
-// const defaultOption = options[2];
+// const filterStocks = (stocks, query) => {
+//   if (!query) {
+//     return stocks;
+//   }
 
-const App = () => {
-  const { search } = window.location;
-  const query = new URLSearchParams(search).get("s");
-  const [searchQuery, setSearchQuery] = useState(query || "");
-  const filteredStocks = filterStocks(stocks, searchQuery);
+//   return stocks.filter((post) => {
+//     const postName = post.name.toLowerCase();
+//     return postName.includes(query);
+//   });
+// };
+
+
+// const App = () => {
+//   const { search } = window.location;
+//   const query = new URLSearchParams(search).get("s");
+//   const [searchQuery, setSearchQuery] = useState(query || "");
+//   const filteredStocks = filterStocks(stocks, searchQuery);
+
+//   return (
+//     <div>
+//       <Search searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
+//       {/* <Dropdown options={options} value={defaultOption} placeholder="Select an option" />; */}
+//       <ul>
+//         {filteredStocks.map((stock) => (
+//           <p key={stock.key}>
+//             {stock.name}
+//             <Sentiment sentiment={stock.sentiment} />
+//           </p>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// };
+
+function App() {
+  // const { search } = window.location;
+  // const query = new URLSearchParams(search).get("s");
+  // const [searchQuery, setSearchQuery] = useState(query || "");
+  const [stocks, setStocks] = useState([]);
+  const [sentiment, setSentiment] = useState([]);
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:5000/sentiment").then(response =>
+    response.json().then(data => {
+      setStocks(data.sentiment)
+    })
+    );
+  }, []);
 
   return (
-    <div>
-      <Search searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
-      {/* <Dropdown options={options} value={defaultOption} placeholder="Select an option" />; */}
-      <ul>
-        {filteredStocks.map((stock) => (
-          <p key={stock.key}>
-            {stock.name}
-            <Sentiment sentiment={stock.sentiment} />
-          </p>
-        ))}
-      </ul>
-    </div>
-  );
-};
+    <Container style={{alignItems: 'center'}}>
+      <Search/>
+    </Container>
+  )
+}
 
 export default App;
--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -1,15 +1,53 @@
-const Search = ({ searchQuery, setSearchQuery }) => (
-    <form action="/" method="get">
-      <input
-        value={searchQuery}
-        onInput={(e) => setSearchQuery(e.target.value)}
-        type="text"
-        id="header-search"
+import React, { useState } from "react";
+import { Button, Dropdown, Form, Input } from "semantic-ui-react";
+
+// const options = [
+//   { value: 'past year', text: 'Past year' },
+//   { value: 'past 3 months', text: 'Past 3 months'},
+//   { value: 'past month', text: "Past month"},
+//   { value: 'past week', text: "Past week"},
+// ];
+
+// const defaultOption = options[2];
+
+// const TimeDropdown = () => (
+// <Dropdown
+//   placeholder='Select time'
+//   selection
+//   options={options}
+// />
+// )
+
+const Search = () => {
+  const [stock, setStock] = useState('');
+  // const [time, setTime] = useState('');
+  const time = 'past month'
+
+  return (
+    <Form>
+      <Form.Field>
+        <Input 
         placeholder="Search stocks"
+        value={stock}
+        onChange={e => setStock(e.target.value)}
         name="s"
-      />
-      <button type="submit">Search</button>
-    </form>
-  );
-  
-  export default Search;
+        />
+      </Form.Field>
+      <Form.Field>
+        <Button onClick={async () => {
+            const query = {time, stock};
+            const response = await fetch('http://127.0.0.1:5000/sentiment', {
+              method: 'GET',
+              body: JSON.stringify(query)
+            })
+
+            if (response.ok) {
+              console.log("response worked")
+            }
+        }}>Submit</Button>
+      </Form.Field>
+    </Form>
+  )
+}
+
+export default Search;
